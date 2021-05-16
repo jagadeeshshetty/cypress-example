e42e57ce5fa852caf3ed4459b39d4dad17589fe5
JC <jagadeesh.cheluvaraj@gmail.com>
2021-05-17 02:11:39 +0530

    added assertion.

--- cypress/integration/the-internet/the-internet.spec.js
+++ cypress/integration/the-internet/the-internet.spec.js
@@ -1,19 +1,30 @@
 
 describe('Test Login', () => {
 
-    it('1. Invoke \'the-internet\' URL.', () => {
+    it('Verify login with valid credentials.', () => {
+        // 1. Invoke \'the-internet\' URL.
         cy.visit('http://the-internet.herokuapp.com/login');
-    });
-
-    it('2. Enter valid usernamd.', () => {
+        // 2. Enter valid usernamd.
         cy.get('#username').type('tomsmith');
-    });
-
-    it('3. Enter valid password.', () => {
+        // 3. Enter valid password.
         cy.get('#password').type('SuperSecretPassword!');
+        // 4. Click on Login button.
+        cy.get('.radius').click();
+        // 5. Verify 'You logged into a secure area!' message appears.
+        cy.get('.flash.success').should('be.visible');
     });
 
-    it('4. Click on Login button.', () => {
+    it('Verify login with invalid credentials.', () => {
+        // 1. Invoke \'the-internet\' URL.
+        cy.visit('http://the-internet.herokuapp.com/login');
+        // 2. Enter valid usernamd.
+        cy.get('#username').type('invalid');
+        // 3. Enter valid password.
+        cy.get('#password').type('invalid');
+        // 4. Click on Login button.
         cy.get('.radius').click();
+        // 5. Verify 'You logged into a secure area!' message appears.
+        cy.get('.flash.success').should('be.visible');
     });
+
 });
